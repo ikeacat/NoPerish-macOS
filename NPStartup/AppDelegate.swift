31d6a6c331707ada6f134c019692d4495e0c2541
ikeacat <mfryk268@gmail.com>
2022-06-08 13:21:12 -0500

    License stuff (headers & move file to project root.

--- NPStartup/AppDelegate.swift
+++ NPStartup/AppDelegate.swift
@@ -2,7 +2,8 @@
 //  AppDelegate.swift
 //  NPStartup
 //
-//  Created by Mason Frykman on 6/7/22.
+//  The project, NoPerish, which includes and is not limited to this source code file is licensed under the GNU General Public License v3.0
+//  Get a copy at https://www.gnu.org/licenses/gpl-3.0.en.html or in the included LICENSE file.
 //
 
 import Cocoa
